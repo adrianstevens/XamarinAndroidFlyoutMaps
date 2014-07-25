9d63ef63f335abee77692d2a385765819a321e21
Adrian Stevens <adrian.stevens@gmail.com>
2014-07-25 08:44:17 -0700

    checkin

---
+++ ActionBarMap/Fragments/MyMapFragment.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+using GooglePlusSignIn;
+
+using Android.Gms.Maps;
+
+
+namespace ICS
+{
+	public class MyMapFragment : Fragment
+	{
+		SupportMapFragment _myMapFrag;
+
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your fragment here
+		}
+
+		public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
+		{
+			//this.HasOptionsMenu = true;
+
+			var view = inflater.Inflate(Resource.Layout.map_frag, null);
+
+			CreateMap ();
+
+			return view;
+		}
+
+		void CreateMap ()
+		{
+			var mapFrag = new SupportMapFragment ();
+
+			_myMapFrag = SupportMapFragment.NewInstance();
+			//FragmentTransaction tx = FragmentManager.BeginTransaction();
+
+			var tx = ChildFragmentManager.BeginTransaction ();
+			tx.Add(Resource.Id.frameMap, _myMapFrag);
+			tx.Commit();
+
+		}
+
+	/*	public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+		{
+			inflater.Inflate(Resource.Menu.refresh, menu);
+		}*/
+	}
+}
+
---
+++ ActionBarMap/MainActivity.cs
@@ -0,0 +1,115 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using System.Collections.Generic;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V4.App;
+using Android.Content.Res;
+using ICS;
+using Android.Gms.Maps;
+
+namespace GooglePlusSignIn
+{
+	[Activity (Label = "ActionBarCompat", Icon = "@drawable/ic_launcher", Theme = "@style/Theme.AppCompat.Light", MainLauncher = true)]
+	//[MetaData ("android.support.UI_OPTIONS", Value = "splitActionBarWhenNarrow")]//If you wanted to slit it!
+	public class MainActivity : ActionBarActivity
+	{
+		static readonly string Tag = "Flyout";
+		DrawerLayout drawerLayout;
+		ActionBarDrawerToggle drawerToggle;
+		ListView drawerList;
+		static string[] sections = new[] { "Map1", "Map2", "Map3" };
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
+
+			drawerLayout = FindViewById<DrawerLayout> (Resource.Id.drawer_layout);
+			drawerLayout.SetDrawerShadow (Resource.Drawable.drawer_shadow, (int)GravityFlags.Start);
+			drawerList = FindViewById<ListView> (Resource.Id.flyout);
+			drawerList.Adapter = new ArrayAdapter<string> (this, Resource.Layout.drawer_list_item, sections);
+
+			drawerToggle = new ActionBarDrawerToggle (this, drawerLayout, Resource.Drawable.ic_drawer, Resource.String.drawer_open, Resource.String.drawer_close);
+			drawerLayout.SetDrawerListener (drawerToggle);
+
+			drawerList.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => ListItemClicked (e.Position);
+			ListItemClicked (0);
+
+			ActionBar.SetDisplayHomeAsUpEnabled (true);
+			ActionBar.SetHomeButtonEnabled (true);
+		}
+
+		void ListItemClicked (int position)
+		{
+			SupportFragmentManager.PopBackStack (null, (int)PopBackStackFlags.Inclusive);
+
+			Android.Support.V4.App.Fragment fragment = null;
+
+			fragment = new MyMapFragment ();
+
+			switch (position) 
+			{
+			case 0:
+				fragment = new MyMapFragment2 ();
+				break;
+			case 1:
+				fragment = new MyMapFragment ();
+				break;
+			default:
+				fragment = new SupportMapFragment();
+
+				break;
+			}
+
+			// Insert the fragment by replacing any existing fragment
+			SupportFragmentManager.BeginTransaction ()
+				.Replace (Resource.Id.content_frame, fragment)
+				.Commit ();
+
+			// Highlight the selected item, update the title, and close the drawer
+			drawerList.SetItemChecked (position, true);
+			SupportActionBar.Title = sections [position];
+			drawerLayout.CloseDrawer (drawerList);
+		}
+
+		protected override void OnPostCreate (Bundle savedInstanceState)
+		{
+			base.OnPostCreate (savedInstanceState);
+			drawerToggle.SyncState ();
+		}
+		public override void OnConfigurationChanged (Configuration newConfig)
+		{
+			base.OnConfigurationChanged (newConfig);
+			drawerToggle.OnConfigurationChanged (newConfig);
+		}
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			if (drawerToggle.OnOptionsItemSelected (item))
+				return true;
+
+			return base.OnOptionsItemSelected (item);
+		}
+		public override bool OnPrepareOptionsMenu (IMenu menu)
+		{
+			var drawerOpen = drawerLayout.IsDrawerOpen (drawerList);
+			//when open don't show anything
+			for (int i = 0; i < menu.Size (); i++)
+				menu.GetItem (i).SetVisible (!drawerOpen);
+
+			return base.OnPrepareOptionsMenu (menu);
+		}
+	}
+}
+
+
